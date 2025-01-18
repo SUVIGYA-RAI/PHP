ea41c12ee02b818fe45a6c6f7d6d807e48c56aba
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2025-01-18 23:16:34 +0530

    Add files via upload

---
+++ IFElse.php
@@ -0,0 +1,11 @@
+<?php
+if($a>=0 && $a<20){
+    echo "Number lies between 0 and 20";
+}
+else if ($a>20 && $a<=50){
+    echo "Number lies between 20 and 50";
+}
+else{
+    echo "Number isn't in range\n";
+}
+?>
---
+++ datatypes.php
@@ -0,0 +1,19 @@
+<?php
+define("Greeting","Welcome to our website\n");
+echo Greeting;
+//Typecasting
+$a=5;
+$f1=(float)$a;
+echo gettype($f1)."\n";//gives double
+echo var_dump($f1);//gives float
+echo $f1."\n";
+
+$dec=5.64;
+$b=(integer)$dec;
+echo gettype($b)."\n";
+echo $b."\n";
+
+$str=" ";
+$tt=(bool)$str;
+echo var_dump($tt);
+?>
