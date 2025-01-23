9fcb53944fc9d06b2ebc90cbefc65dd5f0146bca
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2025-01-23 23:05:45 +0530

    arr for while do while loop

---
+++ Array1.php
@@ -0,0 +1,32 @@
+<?php
+$arr=[1,2,3,4,5,6,7,8,9];
+$sum=0;
+$length=count($arr);//to get the length of the array
+for($i=0;$i<$length;$i++){
+    $sum+=$arr[$i];
+}
+echo "Sum: $sum"."\n";
+$len=(int)readline("Enter the length of the array: ");//reading user input
+$arr1=[];
+for($i=0;$i<$len;$i++){
+    $arr1[$i]=(int)readline("Enter value: ");
+}
+for($i=0;$i<$len;$i++){
+    echo $arr1[$i]." ";
+}
+$i=0;
+while($i<$len){
+    echo $arr1[$i];
+    $i++;
+}echo "\n";
+$j=0;
+do{
+    echo $arr1[$j];
+    $j++;
+}while($j<$len);
+echo "\n";
+
+foreach($arr1 as $k){
+    echo $k." ";
+}
+?>
