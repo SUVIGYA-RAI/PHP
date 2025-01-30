500efe46454fdd22997a9cb806b243c7a6f689cf
Suvigya Rai <147190603+SUVIGYA-RAI@users.noreply.github.com>
2025-01-30 22:41:31 +0530

    break

---
+++ Break_Switch.php
@@ -0,0 +1,17 @@
+<?php
+$num=450;
+switch($num){
+    case 100:
+        echo("Number is equal to 100.");
+        break;
+    case 200:
+        echo("Number is equal to 200.");
+        break;
+    case 50:
+        echo("Number is equal to 50.");
+        break;
+    default:
+    echo("Number is not equal to 100,200,50");
+    break;
+}
+?>
